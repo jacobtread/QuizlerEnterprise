432945f394385d407b70033f4f4913f93bdf1f3b
Jacobtread <jacobtread@gmail.com>
2024-01-30 11:17:13 +1300

    feat: console trace subscriber for dev

--- services/backend/main-node/src/utils/tracing.rs
+++ services/backend/main-node/src/utils/tracing.rs
@@ -1,16 +1,44 @@
-use anyhow::{anyhow, Context};
-use opentelemetry::KeyValue;
-use opentelemetry_otlp::WithExportConfig;
-use opentelemetry_sdk::{trace, trace::BatchConfig, Resource};
 use tracing::level_filters::LevelFilter;
-use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt, Layer};
-
-const SERVICE_NAME: &str = "main-node";
-const TELEMETRY_COLLECTOR_ENDPOINT: &str = "TELEMETRY_COLLECTOR_ENDPOINT";
 
 /// Initializes the OpenTelemetry tracing
 pub fn init_tracing() -> anyhow::Result<()> {
+    let filter = EnvFilter::from_default_env().add_directive(LevelFilter::WARN.into());
+
+    #[cfg(all(feature = "tracing-opentelemetry", not(feature = "tracing-console")))]
+    {
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer().with_filter(filter))
+            .with(create_open_telemetry())
+            .try_init()?;
+    }
+
+    #[cfg(all(feature = "tracing-console", not(feature = "tracing-opentelemetry")))]
+    {
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer().with_filter(filter))
+            .with(console_subscriber::spawn())
+            .try_init()?;
+    }
+
+    Ok(())
+}
+
+/// Creates the open telemetry tracing layer
+#[cfg(feature = "tracing-opentelemetry")]
+fn create_open_telemetry() -> anyhow::Result<tracing_opentelemetry::OpenTelemetryLayer<_, _>> {
+    use anyhow::{anyhow, Context};
+    use opentelemetry::KeyValue;
+    use opentelemetry_otlp::WithExportConfig;
+    use opentelemetry_sdk::{
+        trace::{self, BatchConfig},
+        Resource,
+    };
+    use tracing_opentelemetry::OpenTelemetryLayer;
+
+    const SERVICE_NAME: &str = "main-node";
+    const TELEMETRY_COLLECTOR_ENDPOINT: &str = "TELEMETRY_COLLECTOR_ENDPOINT";
+
     // Telemetry collector endpoint
     let endpoint: String = std::env::var(TELEMETRY_COLLECTOR_ENDPOINT)
         .map_err(|err| anyhow!("Failed to determine telemetry endpoint: {err}"))?;
@@ -37,12 +65,5 @@ pub fn init_tracing() -> anyhow::Result<()> {
         .install_batch(opentelemetry_sdk::runtime::Tokio)
         .context("Creating OpenTelemetry config")?;
 
-    let filter = EnvFilter::from_default_env().add_directive(LevelFilter::WARN.into());
-
-    tracing_subscriber::registry()
-        .with(OpenTelemetryLayer::new(tracer))
-        .with(tracing_subscriber::fmt::layer().with_filter(filter))
-        .try_init()?;
-
-    Ok(())
+    Ok(OpenTelemetryLayer::new(tracer))
 }
