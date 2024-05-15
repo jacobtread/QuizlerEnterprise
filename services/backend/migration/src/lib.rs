01d09451633aea53377c3df3a3f11ff14683c7d6
Jacobtread <jacobtread@gmail.com>
2024-05-15 18:41:30 +1200

    fix: migrations order

--- services/backend/migration/src/lib.rs
+++ services/backend/migration/src/lib.rs
@@ -15,8 +15,8 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20240128_142240_create_quiz_table::Migration),
             Box::new(m20240128_142246_create_users_table::Migration),
+            Box::new(m20240128_142240_create_quiz_table::Migration),
             // Box::new(m20240128_142254_create_analytics_table::Migration),
             // Box::new(m20240128_142337_create_active_quiz_table::Migration),
             // Box::new(m20240128_142720_create_permissions_table::Migration),
